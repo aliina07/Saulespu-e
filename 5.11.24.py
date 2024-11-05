29ab689caeede52bf9b66a88691a6d0091d21219
aliina07 <alinasaprovska07@gmail.com>
2024-11-05 09:17:22 +0200

    Create 5.11.24.py

---
+++ 5.11.24.py
@@ -0,0 +1,10 @@
+class Putns():
+    def __init__(self, krasa,vide,lido):
+        self.krasa = krasa
+        self.vide = vide
+        self.lido = bool(lido) # boll pārveido mainīgo no datu tipu boolen (true and false)
+
+zvirbulis = Putns("pelēka","pļava",True)   
+print(zvirbulis.krasa)
+print(zvirbulis.vide)
+print(zvirbulis.lido)     
